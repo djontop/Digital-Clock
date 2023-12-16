42a953d406883b77b79e55f166c722c663be0795
Manthan Vasant <djontopmv@gmail.com>
2023-12-16 20:40:56 +0530

    Modify Css, js script

--- script.js
+++ script.js
@@ -1,12 +1,74 @@
-function updateClock() {
-    const now = new Date();
-    const hours = now.getHours().toString().padStart(2, '0');
-    const minutes = now.getMinutes().toString().padStart(2, '0');
-    const seconds = now.getSeconds().toString().padStart(2, '0');
+const formatSwitchBtn = document.querySelector(".format-switch-btn");
 
-    const timeString = `${hours}:${minutes}:${seconds}`;
-    document.getElementById('clock').innerText = timeString;
+formatSwitchBtn.addEventListener("click", () => {
+    formatSwitchBtn.classList.toggle("active");
+
+    var formatValue = formatSwitchBtn.getAttribute("data-format");
+
+    if(formatValue === "12"){
+        formatSwitchBtn.setAttribute("data-format", "24");
+    }
+    else{
+        formatSwitchBtn.setAttribute("data-format", "12");
+    }
+});
+
+function clock(){
+    var today = new Date();
+
+    var hours = today.getHours();
+    var minutes = today.getMinutes();
+    var seconds = today.getSeconds();
+    let period = "AM";
+
+    if(hours >= 12){
+        period = "PM";
+    }
+    
+    var formatValue = formatSwitchBtn.getAttribute("data-format");
+
+    if(formatValue == "12"){
+        hours = hours  > 12 ? hours % 12 : hours;
+    }
+
+    if(hours < 10){
+        hours = "0" + hours;
+    }
+    if(minutes < 10){
+        minutes = "0" + minutes;
+    }
+    if(seconds < 10){
+        seconds = "0" + seconds;
+    }
+
+
+    document.querySelector(".hours").innerHTML = hours;
+    document.querySelector(".minutes").innerHTML = minutes;
+    document.querySelector(".seconds").innerHTML = seconds;
+    document.querySelector(".period").innerHTML = period;
 }
+var updateClock = setInterval(clock, 1000);
 
-setInterval(updateClock, 1000);
-updateClock();
+var today = new Date()
+const dayNumber = today.getDate();
+const year = today.getFullYear();
+const dayName = today.toLocaleDateString("default", {weekday: "long"});
+const monthName = today.toLocaleDateString("default", {month: "short"});
+
+document.querySelector(".day-name").innerHTML = dayName;
+document.querySelector(".day-number").innerHTML = dayNumber;
+document.querySelector(".month-name").innerHTML = monthName;
+document.querySelector(".year").innerHTML = year;
+
+const dotMenuBtn = document.querySelector(".dot-menu-btn");
+const dotMenu = document.querySelector(".dot-menu");
+
+dotMenuBtn.addEventListener("click", () => {
+    dotMenu.classList.toggle("active");
+})
+
+document.addEventListener("click", (e) => {
+    if(e.target.id !== "active-menu"){
+        dotMenu.classList.remove("active");
+    }
+})
